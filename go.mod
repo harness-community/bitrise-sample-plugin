b9025df8f603c131011505e65f29d10706e24271
Devansh Mathur <devanshmathur@harness.io>
2025-01-28 13:51:42 +0530

    Custom bitrise step.

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/bitrise-steplib/bitrise-step-android-lint
+
+go 1.16
+
+require (
+	github.com/bitrise-io/go-steputils v0.0.0-20210819160244-b3962254d553
+	github.com/bitrise-io/go-utils v0.0.0-20210819143908-bbd923881fab
+)
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+
+	"github.com/bitrise-io/go-steputils/stepconf"
+	"github.com/bitrise-io/go-utils/log"
+)
+
+// Config ...
+type Config struct {
+	ProjectLocation   string `env:"project_location,dir"`
+	ReportPathPattern string `env:"report_path_pattern"`
+	Variant           string `env:"variant"`
+	Module            string `env:"module"`
+	Arguments         string `env:"arguments"`
+	CacheLevel        string `env:"cache_level,opt[none,only_deps,all]"`
+	DeployDir         string `env:"BITRISE_DEPLOY_DIR,dir"`
+}
+
+var logger = log.NewLogger(false)
+
+func failf(f string, args ...interface{}) {
+	logger.Errorf(f, args...)
+	os.Exit(1)
+}
+
+func main() {
+	var config Config
+
+	if err := stepconf.Parse(&config); err != nil {
+		failf("Process config: couldn't create step config: %v\n", err)
+	}
+
+	stepconf.Print(config)
+	logger.Donef("  Done")
+}
